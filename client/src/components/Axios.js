8328a5c24f6416afe6c683957744d5e059c44694
boubaker achkhbar <boubakerachkhbar3@gmail.com>
2023-09-01 18:27:59 +0100

    change baseurl2

--- client/src/components/Axios.js
+++ client/src/components/Axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const token = localStorage.getItem("token");
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://127.0.0.1:5000/",
 });
 axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
