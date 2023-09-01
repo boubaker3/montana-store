5fd024728c8aad89959515f0dccad0d755765aaa
boubaker achkhbar <boubakerachkhbar3@gmail.com>
2023-09-01 23:07:27 +0100

    log errors2

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 app.use((err, req, res, next) => {
-  res.status(500).send({ message: err.message });
+  console.log(err.message);
 });
 
 app.listen(process.env.PORT || port, () => {
