20e2728ddbf2a805ae1d15534dc70a39bf7b9127
boubaker achkhbar <boubakerachkhbar3@gmail.com>
2023-09-02 18:41:21 +0100

    init12

--- server/index.js
+++ server/index.js
@@ -32,10 +32,11 @@ const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
+
 // Serve static files for React client
-app.use(express.static("../client/build"));
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
 app.listen(process.env.PORT || port, () => {
