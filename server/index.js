98f803f1feddeb00e5a54f18b7b126cd35baeae7
boubaker achkhbar <boubakerachkhbar3@gmail.com>
2023-09-03 16:14:01 +0100

    init21

--- server/index.js
+++ server/index.js
@@ -34,9 +34,9 @@ connection.once("open", () => {
 });
 
 // Serve static files for React client
-app.use(express.static("../client/build"));
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
 app.listen(process.env.PORT || port, () => {
