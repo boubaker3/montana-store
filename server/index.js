843b89522663efb2eb45370cd2a317af4b25c107
boubaker achkhbar <boubakerachkhbar3@gmail.com>
2023-09-04 00:35:57 +0100

    test

--- server/index.js
+++ server/index.js
@@ -1,38 +1,13 @@
 require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
+ 
 const app = express();
 const path = require("path");
-const cors = require("cors");
+ 
 const port = 5000;
-
-// Enable CORS before handling API routes
-app.use(cors());
-
-app.use(express.json());
-
-// API routes setup
-const authRouter = require("./routes/Users");
-const cartRouter = require("./routes/Cart");
-const ordersRouter = require("./routes/Orders");
+ 
 const reviewsRouter = require("./routes/Reviews");
-const contactsRouter = require("./routes/Contacts");
-
-app.use("/", authRouter);
-app.use("/cart", cartRouter);
-app.use("/orders", ordersRouter);
-app.use("/reviews", reviewsRouter);
-app.use("/contacts", contactsRouter);
-
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-const connection = mongoose.connection;
-connection.once("open", () => {
-  console.log("MongoDB database connection established successfully");
-});
-
+ 
 // Serve static files for React client
 app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("*", (req, res) => {
