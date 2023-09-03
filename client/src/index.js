edc9409c3beb63c4cc2275282c6723f0d0bf2511
boubaker achkhbar <boubakerachkhbar3@gmail.com>
2023-09-03 16:33:01 +0100

    init26

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import store from "./components/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter basename="/build/index.html">
+    <BrowserRouter basename="../build/index.html">
       <App />
     </BrowserRouter>
   </Provider>
